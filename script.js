1518ad9e0448659c6c163edf98c24f8aac4676fd
kruzznitsch <rougemeister1@gmail.com>
2024-07-01 15:34:48 +0000

    Added the primary color theme

--- script.js
+++ script.js
@@ -58,3 +58,27 @@ function closeThemeModal(e){
         themeModal.style.display = 'none'
     }
 }
+
+
+const colorPalette = document.querySelectorAll('.choose-color span')
+console.log(colorPalette)
+
+// change primary colors 
+colorPalette.forEach(color => {
+    color.addEventListener('click', ()=>{
+        let primaryHue;
+        if(color.classList.contains('color-1')){
+            primaryHue = 252;
+        }else if(color.classList.contains('color-2')){
+            primaryHue = 52;
+        }else  if(color.classList.contains('color-3')){
+            primaryHue = 352;
+        }else  if(color.classList.contains('color-4')){
+            primaryHue = 152
+        }else  if(color.classList.contains('color-5')){
+            primaryHue = 202
+        }
+
+        root.style.setProperty('--primary-color-hue', primaryHue)
+    })
+})
